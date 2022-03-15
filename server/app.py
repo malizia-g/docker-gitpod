da3b1873d2edf8038a1979a2420554d93c6b7847
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2022-03-15 17:26:11 +0000

    aggiunte variabili ambiente

--- server/app.py
+++ server/app.py
@@ -1,8 +1,45 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+#Creo una funzione che posso riciclare ogni volta che devo accedere al DB
+def get_db():
+    client = MongoClient(host='test_mongodb',
+                         port=27017, 
+                         username='root', 
+                         password='pass',
+                        authSource="admin")
+    db = client["animal_db"]
+    return db
+
+#Creo una route per ottenere tutti gli animali
+@app.route('/animals')
+def get_stored_animals():
+    db=""
+    try:
+        db = get_db()
+        _animals = db.animal_tb.find()
+        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+        return jsonify({"animals": animals})
+    except:
+        pass
+    finally:
+        if type(db)==MongoClient:
+            db.close()
+
+#Verifico di poter leggere le variabili d'ambiente impostate nel file docker-compose.yml
+@app.route('/environment')
+def env():
+    return jsonify(
+            {"env":[
+                {"MONGO_INITDB_DATABASE": os.environ["MONGO_INITDB_DATABASE"]},
+                {"MONGO_INITDB_ROOT_USERNAME": os.environ["MONGO_INITDB_DATABASE"]},
+                {"MONGO_INITDB_ROOT_PASSWORD": os.environ["MONGO_INITDB_ROOT_PASSWORD"]}
+            ]})
 
 @app.route('/')
 def ping_server():
