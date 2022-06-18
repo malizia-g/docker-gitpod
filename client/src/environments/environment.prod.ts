1987897634d664a37519b49a18914024efad81a0
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2022-06-18 15:57:44 +0000

    first update

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+//TODO: Descivere in lezione e usare url generato dal deploy (con variabili d'ambiente??)
 export const environment = {
-  production: true
+  production: true,
+  baseUrlServer: 'https://STILL_NOT_HAVE_URL'
 };
