7ea6a933858229049d1e459e7a2e6c1868bb41f1
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2022-06-22 08:57:07 +0000

    removed code to manage the urls

--- client/src/app/animals.service.ts
+++ client/src/app/animals.service.ts
@@ -12,16 +12,16 @@ export class AnimalsService {
   
   //Il metodo fa una chiamata Http al server
   getAnimals() {
-    //return this.http.get<VettAnimal>(environment.baseUrlServer + 'animals');
-    return this.http.get<VettAnimal>('https://8080-maliziag-dockergitpod-6ia3zbqltpp.ws-eu38.gitpod.io/api/animals');
+    //return this.http.get<VettAnimal>(environment.baseUrlServer + '/api/animals');
+    return this.http.get<VettAnimal>('/api/animals');
   }
-
+  
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
   sendNewAnimal(animal : Animal) : Observable<Animal>
   {
-    return this.http.post<Animal>(environment.baseUrlServer + 'newAnimal', animal,this.httpOptions)
+    return this.http.post<Animal>('/api/newAnimal', animal,this.httpOptions)
   }
 }
 
